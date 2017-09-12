3b68867aeabba0ec683ddb63ebf9dc8d30f33df5
chenyu <chenyu01@linghit.com>
2017-09-12 20:29:32 +0800

    增加模板消息推送

---
+++ src/Sdk/WechatTemplate.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoseChan
+ * Date: 2017/9/12 0012
+ * Time: 下午 8:10
+ */
+
+
+namespace Wxmp\Sdk;
+
+use Wxmp\ApiDomain;
+
+class WechatTemplate
+{
+
+    /**
+     * @var \Network\Http
+     */
+    private $network;
+    /**
+     * @var \Uri
+     */
+    private $uri;
+
+    public function __construct($app_id, $app_secret, $uri = "")
+    {
+        $this->network = new \Network\Http();
+
+        $uri = empty($uri) ? ApiDomain::GENERATE_DOMAIN : $uri;
+
+        $this->uri = new \Uri("https://" . $uri);
+    }
+
+    public function send($access_token,$data)
+    {
+        $uri = clone $this->uri;
+
+        $uri->withPath("/cgi-bin/message/template/send?access_token={$access_token}");
+
+//        $data = [
+//            "appid" => $this->app_id,
+//            "secret" => $this->app_secret,
+//            "code" => $code,
+//            "grant_type" => 'authorization_code'
+//        ];
+
+        $result = $this->network->put($uri, $data)->toArray();
+
+        return $result;
+    }
+
+}
